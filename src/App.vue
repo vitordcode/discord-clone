e4e6ce12e9f02795c67759f9fcddb46c512465e7
Vitor de Paula <vitordcode@gmail.com>
2021-08-07 17:50:51 -0300

    subindo dados responsivo

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="font-poppins">
     <Header />
-    <Main />
-    <Footer />
+    <!-- <Main />
+    <Footer /> -->
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Header from '@/components/Header/index.vue'
-import Main from '@/components/Main/index.vue'
-import Footer from '@/components/Footer/index.vue'
+// import Main from '@/components/Main/index.vue'
+// import Footer from '@/components/Footer/index.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
     Header,
-    Main,
-    Footer
+    // Main,
+    // Footer
   }
 });
 </script>
